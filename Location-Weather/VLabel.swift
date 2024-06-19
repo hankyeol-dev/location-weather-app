d46ffdad7fdb41b2e603dc653fd50a1ed56c341f
hankyeol-dev <ddovlek@kakao.com>
2024-06-19 15:54:42 +0900

    feat: CLLocation Authorization based weather app'

---
+++ Location-Weather/VLabel.swift
@@ -0,0 +1,8 @@
+//
+//  VLabel.swift
+//  Location-Weather
+//
+//  Created by 강한결 on 6/19/24.
+//
+
+import Foundation
---
+++ Location-Weather/WeatherModel.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherModel.swift
+//  Location-Weather
+//
+//  Created by 강한결 on 6/19/24.
+//
+
+import Foundation
